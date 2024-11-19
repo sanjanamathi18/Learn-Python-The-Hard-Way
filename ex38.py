ecc6140a78315239d59a4a0c7d78c5437ed1d782
Sanjana Mathi <sanjuvimal@icloud.com>
2024-11-19 22:07:27 +0100

    Saved again

--- ex38.py
+++ ex38.py
@@ -1,30 +1,29 @@
 # Doing things to lists
 from __future__ import annotations
 
-ten_things = 'Apples Oranges Crows Telephone Light Sugar'
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print("Wait there are not 10 things in that list. Let's fix that.")
 
-stuff = ten_things.split(' ')
-more_stuff = ['Day', 'Night', 'Song',
-              'Frisbee', 'Corn', 'Banana', 'Girl', 'Boy']
+stuff = ten_things.split(" ")
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
     next_one = more_stuff.pop()
-    print('Adding: ', next_one)
+    print("Adding: ", next_one)
     stuff.append(next_one)
     print(f"There are {len(stuff)} items now.")
 
-print('There we go: ', stuff)
+print("There we go: ", stuff)
 
 print("Let's do some things with stuff.")
 
-print('What is in the position 1: ', stuff[1])
+print("What is in the position 1: ", stuff[1])
 # reverse order of the list, starts with -1
-print('What is in the last position: ', stuff[-1])
+print("What is in the last position: ", stuff[-1])
 print(stuff.pop())  # removes last items with empty argument
-print('The list separated by space: ', ' '.join(stuff))
-print('#'.join(stuff[3:5]))  # 3:5 range from 3 to 4
+print("The list separated by space: ", " ".join(stuff))
+print("#".join(stuff[3:5]))  # 3:5 range from 3 to 4
 sorted = stuff.sort()
 print(sorted)  # print "none"
 print(stuff)
--- oop_test.py
+++ oop_test.py
@@ -5,48 +5,40 @@ from __future__ import annotations
 import random
 import sys
 from urllib.request import urlopen
-WORD_URL = 'http://learncodethehardway.org/words.txt'
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
-    'class %%%(%%%):':
-        'Make a class named %%% that is-a %%%.',
-    'class %%%(object):\n\tdef__init__(self, ***)':
-        'class %%% has-a __init__ that takes self and *** params.',
-    'class %%%(object):\n\tdef ***(self, @@@)':
-        'class %%% has-a function *** that takes self and @@@ params.',
-    '*** = %%%()':
-        'Set *** to an instance of class %%%.',
-    '***.***(@@@)':
-        'From *** get the *** function, call it with params self, @@@.',
-    "***.*** = '***'":
-        "From *** get the *** attribute and set it to '***'.",
+    "class %%%(%%%):": "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef__init__(self, ***)": "class %%% has-a __init__ that takes self and *** params.",
+    "class %%%(object):\n\tdef ***(self, @@@)": "class %%% has-a function *** that takes self and @@@ params.",
+    "*** = %%%()": "Set *** to an instance of class %%%.",
+    "***.***(@@@)": "From *** get the *** function, call it with params self, @@@.",
+    "***.*** = '***'": "From *** get the *** attribute and set it to '***'.",
 }
 
 # Do they want to drill phrases first
-if len(sys.argv) == 2 and sys.argv[1] == 'english':
+if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 else:
     PHRASE_FIRST = False
 
 # load up the words from the website
 for word in urlopen(WORD_URL).readlines():
-    WORDS.append(str(word.strip(), encoding='utf-8'))
+    WORDS.append(str(word.strip(), encoding="utf-8"))
 
 
 def convert(snippet, phrase):
-    class_names = [
-        w.capitalize() for w in
-        random.sample(WORDS, snippet.count('%%%'))
-    ]
-    other_names = random.sample(WORDS, snippet.count('%%%'))
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("%%%"))
     results = []
     param_names = []
 
-    for i in range(0, snippet.count('%%%')):
+    for i in range(0, snippet.count("%%%")):
         param_count = random.randint(1, 3)
         param_names.append(
-            ','.join(
+            ",".join(
                 random.sample(WORDS, param_count),
             ),
         )
@@ -56,15 +48,15 @@ def convert(snippet, phrase):
 
         # fake class names
         for word in class_names:
-            result = result.replace('%%%', word, 1)
+            result = result.replace("%%%", word, 1)
 
         # fake other names
         for word in other_names:
-            result = result.replace('***', word, 1)
+            result = result.replace("***", word, 1)
 
         # fake parameter lists
         for word in param_names:
-            result = result.replace('@@@', word, 1)
+            result = result.replace("@@@", word, 1)
 
         results.append(result)
 
@@ -85,10 +77,10 @@ try:
 
             print(question)
 
-            input('> ')
+            input("> ")
             print(f"ANSWER: {answer}\n\n")
 except EOFError:
-    print('\nBye')
+    print("\nBye")
 
     # self when defining function
     # self not when calling
